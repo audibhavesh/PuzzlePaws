d0b6853a98b13666fceb5bac9bf137a054f67b4e
audibhavesh <audichyabhavesh33@gmail.com>
2024-07-25 11:06:15 +0530

    Initial Commit

---
+++ Puzzle Paws/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Puzzle Paws
+//
+//  Created by Bhavesh Audichya on 25/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Puzzle Paws/Puzzle_PawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Puzzle_PawsApp.swift
+//  Puzzle Paws
+//
+//  Created by Bhavesh Audichya on 25/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Puzzle_PawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
